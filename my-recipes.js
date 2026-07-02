0341a66c36df326c8124c0eb968d7f4e3a9a9fbc
Bola hany <boba2009issa@gmail.com>
2026-07-02 20:08:57 +0300

    add recipe page design finished

--- my-recipes.js
+++ my-recipes.js
@@ -1,35 +1,21 @@
-async function getMeals() {
-    const response = await fetch(
-        "https://www.themealdb.com/api/json/v1/1/search.php?s="
-    );
-
-    const data = await response.json();
-
-    document.getElementById("box").innerHTML = "";
-
-    for (let i = 0; i < 100; i++) {
-        document.getElementById("box").innerHTML += `
-        <div class="border-4 border-orange-400 rounded-2xl m-5 w-full max-w-72">
-            <img
-                class="overflow-hidden rounded-t-2xl w-full object-cover"
-                src="${data.meals[i].strMealThumb}"
-                alt="${data.meals[i].strMeal}"
-            >
-
+box = document.getElementById("box");
+function showData() {
+    const data = JSON.parse(localStorage.getItem("meals")) || [];
+    
+    for (let i = 0; i < data.length; i++) {
+        box.innerHTML += `
+                    <div class="border-4  border-orange-400  rounded-2xl m-5 w-full max-w-72">
+            <img class="overflow-hidden rounded-t-2xl w-full object-cover" src =${data[i].strMealThumb} >
             <div class="flex flex-col mt-2 mb-2 mr-2 ml-2 gap-3">
-                <p class="inline-block text-orange-400 border-dotted p-2 rounded-2xl border-2 text-2xl capitalize self-center font-extrabold font-serif text-center">
-                    ${data.meals[i].strMeal}
-                </p>
-
-                <a
-                    class="inline-block bg-orange-400 text-white rounded-2xl text-center w-1/2 p-3 font-bold"
-                    href="recipe.html?id=${data.meals[i].idMeal}"
-                >
-                    View Details
-                </a>
+                <p class="inline-block text-orange-400 border-dotted p-2 rounded-2xl border-2 text-2xl capitalize self-center font-extrabold font-serif text-center">${data[i].strMeal}</p>
+                <a class="inline-block bg-orange-400 text-white rounded-2xl text-center w-1/2  p-3 font-bold" href="recipe.html?id=${data[i].idMeal}"> View Details</a>
             </div>
         </div>
-        `;
+        `
     }
+
+
+
 }
-getMeals();
+
+showData();
